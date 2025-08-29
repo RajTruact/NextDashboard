406ead74c26b151149f360677a3f59ddec1236a2
RajTruact <raj@tecghpit.in>
2025-08-29 16:29:52 +0530

    first

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "plus.unsplash.com",
+      "images.unsplash.com",
+      "cdn-icons-png.flaticon.com",
+      "previews.123rf.com",
+      "c8.alamy.com",
+      "www.truact.in",
+    ],
+  },
+};
+
+export default nextConfig;
