c094c7a5b478c58a227a9aa3d4be8c865cae5ac2
RajTruact <raj@tecghpit.in>
2025-09-03 16:34:21 +0530

    c

--- src/components/common/MoonIcon.jsx
+++ src/components/common/MoonIcon.jsx
@@ -2,7 +2,7 @@ import cx from "classnames";
 export const MoonIcon = ({ className, ...rest }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
+    width="25"
     height="24"
     viewBox="0 0 24 24"
     {...rest}
